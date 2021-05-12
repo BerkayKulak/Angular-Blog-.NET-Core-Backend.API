bddee0a40da2485780f6975193eecd0432b30073
Berkay Kulak <beko_468@hotmail.com>
2021-05-12 18:54:09 +0300

    Add project files.

---
+++ AngularBlogCore.API/Controllers/ArticlesController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AngularBlogCore.API.Models;
+using AngularBlogCore.API.Responses;
+
+namespace AngularBlogCore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticlesController : ControllerBase
+    {
+        private readonly UdemyAngularBlogDBContext _context;
+
+        public ArticlesController(UdemyAngularBlogDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Articles
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Article>>> GetArticles()
+        {
+            return await _context.Articles.ToListAsync();
+        }
+
+        [HttpGet("{page}/{pageSize}")]
+        public IActionResult GetArticles(int page=1,int pageSize=5)
+        {
+            System.Threading.Thread.Sleep(2000);
+            try
+            {
+                IQueryable<Article> query;
+                query = _context.Articles.Include(x => x.Category).Include(y => y.Comments)
+                    .OrderByDescending(z => z.PublishDate);
+
+                int totalCount = query.Count();
+
+
+                // 5*(1-1) => 0
+                var articlesResponse = query.Skip((pageSize * (page - 1))).Take(5).ToList().
+                    Select(x => new ArticleResponse()
+                    {
+                        Id = x.Id,
+                        Title = x.Title,
+                        ContentMain = x.ContentMain,
+                        ContentSummary = x.ContentSummary,
+                        Picture = x.Picture,
+                        ViewCount = x.ViewCount,
+                        CommentCount = x.Comments.Count,
+                        Category = new CategoryResponse() { Id = x.Category.Id, Name = x.Category.Name }
+
+
+                    });
+
+                var result = new
+                {
+                    TotalCount = totalCount,
+                    Articles = articlesResponse
+                };
+
+                return Ok(result);
+            }
+            catch (System.Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+           
+
+        }
+
+        // GET: api/Articles/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Article>> GetArticle(int id)
+        {
+            var article = await _context.Articles.FindAsync(id);
+
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            return article;
+        }
+
+        // PUT: api/Articles/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutArticle(int id, Article article)
+        {
+            if (id != article.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(article).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ArticleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Articles
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Article>> PostArticle(Article article)
+        {
+            _context.Articles.Add(article);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetArticle", new { id = article.Id }, article);
+        }
+
+        // DELETE: api/Articles/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Article>> DeleteArticle(int id)
+        {
+            var article = await _context.Articles.FindAsync(id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            _context.Articles.Remove(article);
+            await _context.SaveChangesAsync();
+
+            return article;
+        }
+
+        private bool ArticleExists(int id)
+        {
+            return _context.Articles.Any(e => e.Id == id);
+        }
+    }
+}
